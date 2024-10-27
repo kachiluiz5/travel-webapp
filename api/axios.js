1b96610f7a887ad8c8d3e6a96840d9582a421d6b
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-10-27 03:26:34 +0100

    feat: finished axios config

---
+++ api/axios.js
@@ -0,0 +1,81 @@
+const apiClient = axios.create({
+  baseURL: "http://localhost:2800",
+  withCredentials: true,
+});
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers["Authorization"] = `Bearer ${token}`;
+            return apiClient(originalRequest);
+          })
+          .catch((err) => Promise.reject(err));
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      return new Promise((resolve, reject) => {
+        axios
+          .get("http://localhost:2800/v1/auth/token-refresh")
+          .then(({ data }) => {
+            const accessToken = data.data.accessToken;
+            localStorage.setItem("accessToken", accessToken);
+            apiClient.defaults.headers[
+              "Authorization"
+            ] = `Bearer ${dccessToken}`;
+            originalRequest.headers["Authorization"] = `Bearer ${accessToken}`;
+
+            processQueue(null, accessToken);
+            resolve(apiClient(originalRequest));
+          })
+          .catch((err) => {
+            processQueue(err, null);
+            reject(err);
+          })
+          .finally(() => {
+            isRefreshing = false;
+          });
+      });
+    }
+
+    return Promise.reject(error);
+  }
+);
